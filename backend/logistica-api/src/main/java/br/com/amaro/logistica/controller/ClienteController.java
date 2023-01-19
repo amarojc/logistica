b26b01ea5275c609ff1f4d7ed55983065d0f17b2
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-01-19 20:56:26 -0300

    Data JPA

--- backend/logistica-api/src/main/java/br/com/amaro/logistica/controller/ClienteController.java
+++ backend/logistica-api/src/main/java/br/com/amaro/logistica/controller/ClienteController.java
@@ -2,23 +2,28 @@ package br.com.amaro.logistica.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.amaro.logistica.domain.model.Cliente;
+import br.com.amaro.logistica.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 
+//Gerando um construtor, via lombok, com todas as propriedades de ClienteRepository
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager manager;
-	
+	private ClienteRepository clienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listarClientes(){
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+			return clienteRepository.findAll();
 	}
+	
+	@GetMapping("/nomecliente")
+	public List<Cliente> buscarClientePorNome(){
+		return clienteRepository.findByNomeContaining("ki");
+	}
+	
 }
