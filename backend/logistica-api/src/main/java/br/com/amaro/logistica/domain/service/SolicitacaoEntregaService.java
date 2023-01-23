1e0c4b7c0447820b576c611f618861585b14a386
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-01-23 15:15:25 -0300

    Service Solicitação de Entrega criada

---
+++ backend/logistica-api/src/main/java/br/com/amaro/logistica/domain/service/SolicitacaoEntregaService.java
@@ -0,0 +1,31 @@
+package br.com.amaro.logistica.domain.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.amaro.logistica.domain.model.Cliente;
+import br.com.amaro.logistica.domain.model.Entrega;
+import br.com.amaro.logistica.domain.model.StatusEntrega;
+import br.com.amaro.logistica.domain.repository.EntregaRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class SolicitacaoEntregaService {
+
+	private EntregaRepository entregaRepository;
+	private CatalogoClienteService catalogoClienteService;
+	
+	@Transactional
+	public Entrega solicitarEntrega(Entrega entrega) {
+		Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
+		
+		entrega.setCliente(cliente);
+		entrega.setStatus(StatusEntrega.PENDENTE);
+		entrega.setDataPedido(LocalDateTime.now());
+		
+		return entregaRepository.save(entrega);
+	}
+}
