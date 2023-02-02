5c2ffc6057275e24485bcfbcca7003ba26f59f43
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-02-01 23:39:11 -0300

    Model Ocorrencia iniciada

---
+++ backend/logistica-api/src/main/java/br/com/amaro/logistica/domain/model/Ocorrencia.java
@@ -0,0 +1,35 @@
+package br.com.amaro.logistica.domain.model;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Ocorrencia {
+
+		@EqualsAndHashCode.Include
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@ManyToOne
+		private Entrega entrega;
+		
+		private String descricao;
+		private OffsetDateTime dataRegistro;
+		
+		public Ocorrencia(){
+			
+		}
+}
